101d5da6cfca75eb0a8ea4d8cb7bc9fb7c446423
f3sch <f3sch.git@outlook.com>
2022-02-10 19:19:42 +0100

    creating basic repo
    Signed-off-by: f3sch <f3sch.git@outlook.com>

--- src/repo.hpp
+++ src/repo.hpp
@@ -1,14 +1,13 @@
 #pragma once
 
-#include <boost/program_options.hpp>
-#include <exception>
 #include <filesystem>
+#include <fstream>
+#include <iostream>
 
 #include "config.hpp"
 
 namespace libwyag::repo {
 namespace fs = std::filesystem;
-namespace po = boost::program_options;
 
 using namespace libwyag::config;
 using namespace std;
@@ -33,10 +32,40 @@ class GitRepository {
 
   void print_info();
 
+  // utility functions
+  auto repo_path(const fs::path& path) noexcept { return gitdir_ / path; }
+  auto repo_dir(const fs::path& path, bool mkdir = false) {
+    auto rpath = repo_path(path);
+
+    if (fs::exists(rpath)) {
+      if (fs::is_directory(rpath)) {
+        return rpath;
+      } else {
+        auto msg = "Not a directory: ";
+        throw std::runtime_error(msg + rpath.string());
+      }
+    }
+
+    if (mkdir) {
+      fs::create_directory(rpath);
+      return rpath;
+    } else {
+      return fs::path();
+    }
+  }
+  auto repo_file(const fs::path& path, bool mkdir = false) {
+    if (repo_dir(path.parent_path(), mkdir).empty()) {
+      return repo_path(path);
+    }
+    return fs::path();
+  }
+
+  auto get_worktree() { return worktree_; }
+
  private:
-  fs::path worktree_;
-  fs::path gitdir_;
-  fs::path conf_;
+  const fs::path worktree_;
+  const fs::path gitdir_;
+  const fs::path conf_;
   Config config_;
 };
 
@@ -46,6 +75,51 @@ inline void GitRepository::print_info() {
   cout << "Config: " << conf_ << endl;
   cout << "Repo Info:" << endl;
   config_.print_info();
+}
+
+inline auto repo_create(const fs::path& path) {
+  auto repo = GitRepository(path, true);
+  auto wktree = repo.get_worktree();
+
+  // make sure path does not exists or is an empty directory
+  if (fs::exists(wktree)) {
+    if (not fs::is_directory(wktree)) {
+      auto msg = " is not a directory!";
+      throw std::runtime_error(wktree.string() + msg);
+    }
+    if (not fs::is_empty(wktree)) {
+      auto msg = " is not an empty directory!";
+      throw std::runtime_error(wktree.string() + msg);
+    }
+  } else {
+    // create if it does not exist
+    fs::create_directory(wktree);
+  }
+
+  // create subdirs
+  repo.repo_dir("branches", true);
+  repo.repo_dir("objects", true);
+  repo.repo_dir("refs", true);
+  repo.repo_dir("refs/tags", true);
+  repo.repo_dir("refs/heads", true);
+
+  ofstream file;
+  // .git/description
+  file.open(repo.repo_file("description"));
+  file << "Unnamed repository; edit this file 'description' to name the "
+          "repository.\n";
+  file.close();
+
+  // .git/HEAD
+  file.open(repo.repo_file("HEAD"));
+  file << "ref: refs/heads/master\n";
+  file.close();
+
+  // write config
+  configuration cfg;
+  cfg.write(repo.repo_file("config"));
+
+  return repo;
 }
 
 }  // namespace libwyag::repo
