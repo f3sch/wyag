9a02efca3f1152fd2b879c0f48e78f6f9ca78d03
f3sch <f3sch.git@outlook.com>
2022-02-13 13:18:00 +0100

    cli docs
    Signed-off-by: f3sch <f3sch.git@outlook.com>

--- src/cli.hpp
+++ src/cli.hpp
@@ -9,24 +9,51 @@ namespace po = boost::program_options;
 namespace fs = std::filesystem;
 using namespace std;
 
+/**
+ * Represent the init command and its optionals.
+ */
 struct Init {
-  fs::path path_;
+  fs::path path_; /* Where to initialize repository. */
 };
 
+/**
+ * Wrapper for all positional arguements.
+ * @note int is a placeholder, since it is easy to initialize.
+ */
 typedef variant<int, Init> Command;
 
+/**
+ * Represents the command line interface.
+ * It provides a parse() function to try parsing.
+ */
 class Cli {
  public:
+  /**
+   * Only Cunstructor for this class.
+   * @param argc Is the count of given command line arguements
+   * @param argv C-sytle array of argument list
+   */
   Cli(const int argc, const char* argv[]);
 
+  /**
+   * Parsing function.
+   * @warning May throw!
+   *
+   * @return A std::variant of all options.
+   */
   Command parse();
 
  private:
-  po::options_description global_{"Allowed global options."};
-  po::positional_options_description pos_;
-  variant<int, po::parsed_options> parsed_{0};
-  po::variables_map vm_;
+  po::options_description global_{
+      "Allowed global options."}; /* Global argument description. */
+  po::positional_options_description
+      pos_; /* Position arguemnts given via cli. */
+  variant<int, po::parsed_options> parsed_{0}; /* Parsed arguements. */
+  po::variables_map vm_; /* Storage for arguements after parsing. */
 
+  /**
+   * In case of error, print help message
+   */
   void print_help();
 };
 
